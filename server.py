9eb9bb49219e84d9be2cd4356575d0b816d20a55
rufatbayramov90 <78435344+rufatbayramov90@users.noreply.github.com>
2024-01-15 23:44:33 +0400

    multiline statments

--- server.py
+++ server.py
@@ -24,6 +24,15 @@ def guess(name):
     age = age_data["age"]
     return render_template("guess.html", person_name=name, gender=gender, age=age)
 
+
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
